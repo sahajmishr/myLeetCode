8721e17357f569b750bf62757efbc184bc615017
sahajmishr <mishrasahaj88@gmail.com>
2026-08-17 23:29:45 +0530

    Time: 0 ms (100%), Space: 15.7 MB (81.52%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-       int index = -1;
-
-        // inspired by og eeshan , th echitty , shamsher pro max , guy with red bass .. 
-        for (int i = nums.size() - 2; i >= 0; i--) {
-            
-            if (nums[i] < nums[i + 1]) {
-           
-                index = i;
+        
+        int n = nums.size(), index = -1 ;
+        for(int i = n-2 ; i>=0 ; i--){
+            if(nums[i]<nums[i+1]){
+                index = i ;
                 break;
             }
         }
-
-        
-        if (index == -1) {
-          
+        if(index == -1 ){
             reverse(nums.begin(), nums.end());
-            return;
+            return ;
         }
-
-        
-        for (int i = nums.size() - 1; i > index; i--) {
-           
-            if (nums[i] > nums[index]) {
-                swap(nums[i], nums[index]);
+        for(int j = n-1 ; j>=index ; j-- ){
+            if(nums[j]>nums[index]){
+                swap(nums[j], nums[index]);
                 break;
             }
         }
+        reverse(nums.begin()+index+1 , nums.end());
 
-        
-        reverse(nums.begin() + index + 1, nums.end());
     }
 };
