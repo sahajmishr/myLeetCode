41f834dcbf32119c1735fe35a9c41aa00a030edb
sahajmishr <mishrasahaj88@gmail.com>
2026-08-18 15:23:36 +0530

    Time: 0 ms (100%), Space: 9.4 MB (57.64%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,51 +1,38 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> result;
+
+        /// hey mate you triggered the wrong one ...... the wrong wrong version of me ;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int top = 0 , bottom = m-1 ; 
+        int left = 0 , right = n-1 ; 
+        vector<int> ot;
+        while(left<=right&& top<=bottom){
+        for(int i = left ; i<= right ; i++){
+            ot.emplace_back(matrix[top][i]);
+        }
+        top++ ;
+    
+        for(int i = top ; i <= bottom ; i++){
+            ot.emplace_back(matrix[i][right]);
+        }
+        right-- ;
         
-        
-        if (matrix.empty() || matrix[0].empty()) {
-            return result;
+        if(top<= bottom){
+        for(int i = right ;i >= left ; i--){
+            ot.emplace_back(matrix[bottom][i]);
+        }
+        bottom--;
+        }
+        if(left<= right){
+        for(int i = bottom ; i >= top ; i --){
+            ot.emplace_back(matrix[i][left]);
+        }
+        left++ ;
         }
         
-        
-        int top = 0;
-        int bottom = matrix.size() - 1;
-        int left = 0;
-        int right = matrix[0].size() - 1;
-        
-       
-        while (left <= right && top <= bottom) {
-            
-            
-            for (int i = left; i <= right; ++i) {
-                result.emplace_back(matrix[top][i]);
-            }
-            top++; 
-            
-            
-            for (int i = top; i <= bottom; ++i) {
-                result.emplace_back(matrix[i][right]);
-            }
-            right--; 
-            
-       
-            if (top <= bottom) {
-                for (int i = right; i >= left; --i) {
-                    result.emplace_back(matrix[bottom][i]);
-                }
-                bottom--; 
-            }
-            
-            
-            if (left <= right) {
-                for (int i = bottom; i >= top; --i) {
-                    result.emplace_back(matrix[i][left]);
-                }
-                left++; 
-            }
         }
-        
-        return result;
+        return ot ;
     }
 };
