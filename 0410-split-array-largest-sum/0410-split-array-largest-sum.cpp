f2ea76b25706a45f2ba97837236586a62a56b2f9
sahajmishr <mishrasahaj88@gmail.com>
2026-08-24 12:03:37 +0530

    Time: 0 ms (100%), Space: 10.6 MB (10.76%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-  bool canSplit(const std::vector<int>& nums, int k, int maxSum) {
-        int currentSum = 0;
-        int subarraysCount = 1;
-// should be ashamed of me ...mate
-        for (int num : nums) {
-            if (currentSum + num > maxSum) {
-                currentSum = num;
-                subarraysCount++;
-                if (subarraysCount > k) {
-                    return false;
-                }
-            } else {
-                currentSum += num;
-            }
+  int giveMeSum(vector<int>& nums, int cap){
+    int sum = 0 , ks =1 ;
+    for(int i = 0 ; i<nums.size() ; i++){
+        
+        if(sum+nums[i] > cap){
+            ks ++ ;
+            sum = nums[i];
+        }else {
+            sum += nums[i];
         }
-        return true;
+        
     }
-    int splitArray(std::vector<int>& nums, int k) {
-        int low = *std::max_element(nums.begin(), nums.end());
-        int high = std::accumulate(nums.begin(), nums.end(), 0);
-        int ans = high;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (canSplit(nums, k, mid)) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+    return ks ;
+  }
+    int splitArray(vector<int>& nums, int k) {
+     // ava ava ta batayi toha ... hehehehe don by meeeee  
+     int high = accumulate(nums.begin() , nums.end() , 0 ) ;
+     int low = *max_element(nums.begin(), nums.end());
+     int ans = 0  , mid = 0 , pos = 0;
+     while(low<= high){
+          mid = (low+high)/2 ;
+          pos = giveMeSum(nums , mid);
+        if(pos>k){
+            low = mid+1 ;
+        }else {
+            
+            high = mid-1;
         }
-        return ans;
+     }
+     return low ;
+     
     }
 };
