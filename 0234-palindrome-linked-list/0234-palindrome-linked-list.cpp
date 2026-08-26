50fb824826fc8e0a9367d5a9193e8bf0002921b1
sahajmishr <mishrasahaj88@gmail.com>
2026-08-26 06:41:02 +0530

    Time: 1 ms (67.22%), Space: 117.9 MB (81.97%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,21 +10,41 @@
  */
 class Solution {
 public:
+ListNode *reverse(ListNode* head ){
+    if(head==nullptr || head->next == nullptr) return head ;
+    ListNode* next = nullptr;
+    ListNode* prev = nullptr ;
+    while(head){
+        next = head->next;
+        head->next = prev ;
+        prev = head ;
+        head = next ;
+      
+
+    }
+    return prev ;
+
+}
     bool isPalindrome(ListNode* head) {
-       stack<int> st ;
-      ListNode* temp = head;
-       while(temp){
-        st.push(temp->val);
-        temp= temp->next;
-       }
-       // if it worked  i think i am on mhy current track 
-       temp = head ;
-       while(temp){
-        int top = st.top();
-        if(temp->val != top) return false ;
-        temp = temp->next ;
-        st.pop();
-       }
+      // using the O(1)space ;
+      //slow and fast is still in use as i have to find the middle bruuhhh 
+      //i am not sure if it will work or fail lets see
+      if(head->next == nullptr || head == nullptr ) return true ;
+      ListNode* slow = head ;
+      ListNode* fast = head ;
+      while(fast&& fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+      }
+     ListNode* revHead =  reverse(slow);
+     ListNode* temp1 = head ;
+     ListNode* temp2 = revHead ;
+     while(temp2){
+        if(temp1->val!=temp2->val) return false ;
+        temp1 = temp1->next ;
+        temp2 = temp2->next ;
+     }
        return true ;
+
     }
 };
